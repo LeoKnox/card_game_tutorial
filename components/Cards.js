20b29b5f4a41fb4e98ddb59d338a64d62b611160
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-30 17:20:18 -0700

    Update Cards.js

--- components/Cards.js
+++ components/Cards.js
@@ -2,24 +2,26 @@ import { useState } from "react";
 import { Card } from "./Card.js";
 
 export const Cards = () => {
-  const [deck, setDeck] = useState([
-    { id: 1, name: "ao", stat: "" },
-    { id: 1, name: "ao", stat: "" },
-    { id: 2, name: "aka", stat: "" },
-    { id: 2, name: "aka", stat: "" },
-    { id: 3, name: "midori", stat: "" },
-    { id: 3, name: "midori", stat: "" },
-    { id: 4, name: "chairo", stat: "" },
-    { id: 4, name: "chairo", stat: "" },
-    { id: 5, name: "kiiro", stat: "" },
-    { id: 5, name: "kiiro", stat: "" },
-    { id: 6, name: "shiro", stat: "" },
-    { id: 6, name: "shiro", stat: "" },
-    { id: 7, name: "kuro", stat: "" },
-    { id: 7, name: "kuro", stat: "" },
-    { id: 8, name: "haiiro", stat: "" },
-    { id: 8, name: "haiiro", stat: "" },
-  ]);
+  const [deck, setDeck] = useState(
+    [
+      { id: 1, name: "ao", stat: "" },
+      { id: 1, name: "ao", stat: "" },
+      { id: 2, name: "aka", stat: "" },
+      { id: 2, name: "aka", stat: "" },
+      { id: 3, name: "midori", stat: "" },
+      { id: 3, name: "midori", stat: "" },
+      { id: 4, name: "chairo", stat: "" },
+      { id: 4, name: "chairo", stat: "" },
+      { id: 5, name: "kiiro", stat: "" },
+      { id: 5, name: "kiiro", stat: "" },
+      { id: 6, name: "shiro", stat: "" },
+      { id: 6, name: "shiro", stat: "" },
+      { id: 7, name: "kuro", stat: "" },
+      { id: 7, name: "kuro", stat: "" },
+      { id: 8, name: "haiiro", stat: "" },
+      { id: 8, name: "haiiro", stat: "" },
+    ].sort(() => Math.random() - 0.5)
+  );
   return (
     <div className="container">
       {deck.map((item, index) => (
