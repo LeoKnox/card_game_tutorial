7e88d0e557837e0d6a8a02760a401f34291922ef
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-02 15:27:45 -0700

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,7 @@
-export const Card = ({ item }) => {
-  return <div className="card">{item.name}</div>;
+export const Card = ({ item, id, handleClick }) => {
+  return (
+    <div className="card" onClick={() => handleClick(id)}>
+      <label className="singleCard">{item.name}</label>
+    </div>
+  );
 };
