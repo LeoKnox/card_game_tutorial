10f07a23b0b87455ee758a8fb38b86b0d00a8b48
LeoKnox <LeoKnox@users.noreply.github.com>
2024-07-02 15:36:43 -0700

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,7 @@
 export const Card = ({ item, id, handleClick }) => {
+  const itemClass = item.stat ? " active " + item.stat : "";
   return (
-    <div className="card" onClick={() => handleClick(id)}>
+    <div className={"card" + itemClass} onClick={() => handleClick(id)}>
       <label className="singleCard">{item.name}</label>
     </div>
   );
